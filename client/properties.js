e7c82f9dc9c9bc7b0594214f84c0155223f2f6e3
jaytechnos1 <hakansson_falerum@hotmail.com>
2024-11-15 08:17:47 +0100

    Update properties.js

--- client/properties.js
+++ client/properties.js
@@ -5,7 +5,7 @@ const properties = {
     //this empty if connecting to the 
     //host directly.
     
-    address: "",
+    address: "https://http://46.11.11.116/",
     
     //The port the server is running on
     
